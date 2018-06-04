ffa38a62fc7e22c6e952407ce13d16538b57abb8
liyang <1810824959@qq.com>
2018-06-04 16:23:59 +0800

    原本login.py 和 main.py 用的是两套start_settng,现在改成一样的

--- main.py
+++ main.py
@@ -1,27 +1,20 @@
 # author_li
 # create time :2018/4/30
-from shuoshuo import deal_one
-from db_conf.redis_conf.db_conf import *
-import threading
-from login import write_into,start_setting
+from concurrent.futures import ThreadPoolExecutor
 
+from db_conf.redis_conf.db_conf import *
+from login import write_into, start_setting
+from shuoshuo import deal_one
 
 
 def mian():
     write_into()
     deal_one(start_setting['qq_number'])
-    t1 = threading.Thread(target=do_one())
-    t2 = threading.Thread(target=do_one())
-    t3 = threading.Thread(target=do_one())
-    t4 = threading.Thread(target=do_one())
-    t5 = threading.Thread(target=do_one())
-    t6 = threading.Thread(target=do_one())
-    t7 = threading.Thread(target=do_one())
-    t8 = threading.Thread(target=do_one())
-    t_list = [t1,t2,t3,t4,t5,t6,t7,t8]
-    for i in t_list:
-        i.start()
-
+    with ThreadPoolExecutor(max_workers=12) as e:
+        k=0
+        while(k<13):
+            e.submit(do_one)
+            k+=1
 
 
 def do_one():
